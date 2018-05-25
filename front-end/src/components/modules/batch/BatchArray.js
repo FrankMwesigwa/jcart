6c760f2a6c11461abdfff0287dadf3b0a237b35d
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2018-05-25 12:32:49 +0300

    redesigned the Batch Array Forms

--- front-end/src/components/modules/batch/BatchArray.js
+++ front-end/src/components/modules/batch/BatchArray.js
@@ -23,30 +23,56 @@ class BatchArray extends Component {
       }
 
       const renderAccounts = ({ fields, meta: { error, submitFailed } }) => (
-        <ul>
+        <ul >
           <li>
-            <button type="button" onClick={() => fields.push({})} class="btn btn-primary">Add Account Details</button>
+            <button type="button" onClick={() => fields.push({})} class="btn btn-success">Add Account Details</button>
             {submitFailed && error && <span>{error}</span>}
           </li>
           {fields.map((account, index) => (
-            <li key={index}>
-              <button type="button" title="Remove Account" class="btn btn-danger" onClick={() => fields.remove(index)}>Remove Account</button>
-              <h4>Account #{index + 1}</h4>
-                <div class="form-group">
-                    <label>Fisrt Name</label>
-                    <Field name={`${account}.firstName`} component="input" type="text" placeholder="Enter Role Name" class="form-control" />
+    <li key={index}>
+        <div class="box box-default">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Account : {index + 1}</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                 </div>
-                <div class="form-group">
-                    <label>Fisrt Name</label>
-                    <Field name={`${account}.lastName`} component="input" type="text" placeholder="Enter Role Name" class="form-control" />
                 </div>
-            </li>
-          ))}
-        </ul>
-      )
+
+            <div class="box-body">
+                <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Minimal</label>
+                        <Field name={`${account}.firstName`} component="input" type="text" placeholder="Enter Role Name" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Disabled</label>
+                        <Field name={`${account}.lastName`} component="input" type="text" placeholder="Enter Role Name" class="form-control" />
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Multiple</label>
+                        <Field name={`${account}.firstName`} component="input" type="text" placeholder="Enter Role Name" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Disabled Result</label>
+                        <Field name={`${account}.lastName`} component="input" type="text" placeholder="Enter Role Name" class="form-control" />
+                    </div>
+                </div>
+                </div>
+          </div>
+            <div class="box-footer">
+                <button type="button" title="Remove Account" class="btn btn-danger" onClick={() => fields.remove(index)}>Remove Account</button>
+            </div>
+        </div>   
+</li>))}
+</ul>
+)
 
     return (
-      <div>
+    <div>
 
     <section class="content-header">
       <h1>User Batch Management</h1>
@@ -58,30 +84,43 @@ class BatchArray extends Component {
 
   <section class="content container-fluid">
   <div class="box box-warning">
-                <div class="box-header with-border">
-                  <h3 class="box-title">Create New Batch</h3>
-                </div>
-                <div class="box-body">
-
+    <div class="box-header with-border"><h3 class="box-title">Create New Batch</h3></div>
+        <div class="box-body">
             <form onSubmit={handleSubmit}>
+
+                <div class="box-body">
+                <div class="row">
+                <div class="col-md-6">
                     <div class="form-group">
-                        <label>Name</label>
-                        <Field name="clubName" component="input" type="text" class="form-control" />
+                        <label>Minimal</label>
+                        <Field name="name" component="input" type="text" class="form-control" />
                     </div>
                     <div class="form-group">
-                        <label>Name</label>
+                        <label>Disabled</label>
                         <Field name="description" component="input" type="text" class="form-control"/>
                     </div>
-                    <FieldArray name="members" component={renderAccounts} />
-                <div class="box-footer">
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Multiple</label>
+                        <Field name="description" component="input" type="text" class="form-control"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Disabled Result</label>
+                        <Field name="description" component="input" type="text" class="form-control"/>
+                    </div>
+                </div>
+                </div>
+                <FieldArray name="accounts" component={renderAccounts} />
+          </div>
+          <div class="box-footer">
 	                <button type="submit" disabled={submitting} class="btn btn-primary">Save Batch</button>
                     <button type="button" disabled={pristine || submitting} onClick={reset} class="btn btn-primary">Clear Batch</button>
-	            </div>
-          </form>
-                	
-                </div>
-            </div>
+	       </div>
 
+          </form>	
+        </div>
+    </div>
   </section>
 </div>
     )
